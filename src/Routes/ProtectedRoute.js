9e228867232471cb13785fb1c0e5c6f4a4fee2db
Dery922 <franklindery922@gmail.com>
2025-09-18 07:24:49 +0000

    Update ProtectedRoute.js

--- src/Routes/ProtectedRoute.js
+++ src/Routes/ProtectedRoute.js
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-console.log("🔑 Current user in Redux:", user);
-console.log("Role:", user?.role);
+
 
 
 
 const ProtectedRoute = ({allowedRoles, children}) => {
   const user = useSelector((state) => state.user.user);
+  console.log("🔑 Current user in Redux:", user);
+console.log("Role:", user?.role);
     
         if(!user ) {
           return <Navigate to="/login" replace />
